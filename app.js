d8a6d70779de4f770a0d4fe51cc195b2d4d55369
Derek Young <dthomasy@gmail.com>
2016-03-20 23:31:42 -0700

    Add event listeners

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-//var http = require("http");
-
-/* Create an HTTP server to handle responses
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
-*/
-
-/* Code for browser request */
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var url = "https://api.spotify.com/v1/artists/0L8ExT028jH3ddEcZwqJJ5/top-tracks?country=US";
+
+window.onload = addListeners;
+
+function addListeners() {
+  document.getElementById('search').addEventListener("click", function () {
+    console.log("yoooo");
+    getResponse(callResponse);
+    //listSongs(document.getElementById('query').value);
+  });
+}
 
 function getResponse(callback) {
   var oReq = new XMLHttpRequest();
   oReq.onreadystatechange = function () {
-    if (oReq.readyState === 4 && oReq.status === 200) { // successfully
-      callback(oReq.responseText); // we're calling our method
+    if (oReq.readyState === 4 && oReq.status === 200) {
+      callback(oReq.responseText);
     }
   };
   oReq.open('GET', url);
   oReq.send();
 }
 
-getResponse(callResponse);
-
 function callResponse(result) {
   var response = JSON.parse(result);
   response = response.tracks;
-  for(var n in response) console.log(response[n].name);
+  console.log(response[0].name);
+  var resultsPlaceholder = document.getElementById('results');
+  resultsPlaceholder.innerHTML = response[0].name;
+  //for(var n in response) console.log(response[n].name);
 };
-//console.log(response);
